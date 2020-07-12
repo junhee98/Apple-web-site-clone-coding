36c90b4e7c39190683074adcff4c4f007282e2ef
junhee98 <l_junhe98@naver.com>
2020-07-12 23:27:00 +0900

    Apple web site clone coding day9

---
+++ mainpage.js
@@ -0,0 +1,251 @@
+function change_nav(index) {
+  var number = index;
+  switch (number) {
+    case 1 : document.querySelector('.nav-part-mac').style.color = "#fff";
+      break;
+    case 2 : document.querySelector('.nav-part-ipad').style.color = "#fff";
+      break;
+    case 3 : document.querySelector('.nav-part-iphone').style.color = "#fff";
+      break;
+    case 4 : document.querySelector('.nav-part-watch').style.color = "#fff";
+      break;
+    case 5 : document.querySelector('.nav-part-music').style.color = "#fff";
+      break;
+    case 6 : document.querySelector('.nav-part-supportclient').style.color = "#fff";
+      break;
+  }
+}
+
+function origin_nav(index) {
+  var number = index;
+  switch (number) {
+    case 1 : document.querySelector('.nav-part-mac').style.color = "#c6c9cf";
+      break;
+    case 2 : document.querySelector('.nav-part-ipad').style.color = "#c6c9cf";
+      break;
+    case 3 : document.querySelector('.nav-part-iphone').style.color = "#c6c9cf";
+      break;
+    case 4 : document.querySelector('.nav-part-watch').style.color = "#c6c9cf";
+      break;
+    case 5 : document.querySelector('.nav-part-music').style.color = "#c6c9cf";
+      break;
+    case 6 : document.querySelector('.nav-part-supportclient').style.color = "#c6c9cf";
+      break;
+  }
+}
+
+function change_nav_logo() {
+  var access_logo = document.querySelector('.nav-logo');
+  var change_logo = access_logo.querySelector('img');
+  change_logo.src = "apple-brands-white.svg";
+}
+
+function origin_nav_logo() {
+  var access_logo = document.querySelector('.nav-logo');
+  var change_logo = access_logo.querySelector('img');
+  change_logo.src = "apple-brands-nav.svg";
+}
+
+function change_nav_search() {
+  var access_search = document.querySelector('.nav-search');
+  var change_search = access_search.querySelector('img');
+  change_search.src = "search-solid-white.svg";
+}
+
+function origin_nav_search() {
+  var access_search = document.querySelector('.nav-search');
+  var change_search = access_search.querySelector('img');
+  change_search.src = "search-solid.svg";
+}
+
+function change_nav_bag() {
+  var access_bag = document.querySelector('.nav-bag');
+  var change_bag = access_bag.querySelector('img');
+  change_bag.src = "shopping-bag-white.svg";
+}
+
+function origin_nav_bag() {
+  var access_bag = document.querySelector('.nav-bag');
+  var change_bag = access_bag.querySelector('img');
+  change_bag.src = "shopping-bag-solid.svg";
+}
+
+function change_online() {
+  var change_online = document.querySelector('.online-buy');
+  change_online.style.textDecoration = "underline";
+}
+
+function origin_online() {
+  var change_online = document.querySelector('.online-buy');
+  change_online.style.textDecoration = "none";
+}
+
+function change_se() {
+  var change_se = document.querySelector('.iPhone-se-moreinfo');
+  change_se.style.textDecoration = "underline";
+}
+
+function origin_se() {
+  var change_se = document.querySelector('.iPhone-se-moreinfo');
+  change_se.style.textDecoration = "none";
+}
+
+function change_sebuy() {
+  var change_sebuy = document.querySelector('.iPhone-se-buy-part');
+  change_sebuy.style.textDecoration = "underline";
+}
+
+function origin_sebuy() {
+  var change_sebuy = document.querySelector('.iPhone-se-buy-part');
+  change_sebuy.style.textDecoration = "none";
+}
+
+function change_11() {
+  var change_11 = document.querySelector('.iPhone-11-moreinfo');
+  change_11.style.textDecoration = "underline";
+}
+
+function origin_11() {
+  var change_11 = document.querySelector('.iPhone-11-moreinfo');
+  change_11.style.textDecoration = "none";
+}
+
+function change_11buy() {
+  var change_11buy = document.querySelector('.iPhone-11-buy-part');
+  change_11buy.style.textDecoration = "underline";
+}
+
+function origin_11buy() {
+  var change_11buy = document.querySelector('.iPhone-11-buy-part');
+  change_11buy.style.textDecoration = "none";
+}
+
+function change_padpro() {
+  var change_padpro = document.querySelector('.iPad-pro-moreinfo');
+  change_padpro.style.textDecoration = "underline";
+}
+
+function origin_padpro() {
+  var change_padpro = document.querySelector('.iPad-pro-moreinfo');
+  change_padpro.style.textDecoration = "none";
+}
+
+function change_padprobuy() {
+  var change_padprobuy = document.querySelector('.iPad-pro-buy-part');
+  change_padprobuy.style.textDecoration = "underline";
+}
+
+function origin_padprobuy() {
+  var change_padprobuy = document.querySelector('.iPad-pro-buy-part');
+  change_padprobuy.style.textDecoration = "none";
+}
+
+function change_watchwwwdc20() {
+  var change_watchwwwdc20 = document.querySelector('.watch-wwwdc20');
+  change_watchwwwdc20.style.textDecoration = "underline";
+}
+
+function origin_watchwwwdc20() {
+  var change_watchwwwdc20 = document.querySelector('.watch-wwwdc20');
+  change_watchwwwdc20.style.textDecoration = "none";
+}
+
+function change_applesmessage() {
+  var change_applesmessage = document.querySelector('.apples-message');
+  change_applesmessage.style.textDecoration = "underline";
+}
+
+function origin_applesmessage() {
+  var change_applesmessage = document.querySelector('.apples-message');
+  change_applesmessage.style.textDecoration = "none";
+}
+
+function change_iPhoneWatch() {
+  var change_iPhoneWatch = document.querySelector('.iPhone_Watch_moreinfo');
+  change_iPhoneWatch.style.textDecoration = "underline";
+}
+
+function origin_iPhoneWatch() {
+  var change_iPhoneWatch = document.querySelector('.iPhone_Watch_moreinfo');
+  change_iPhoneWatch.style.textDecoration = "none";
+}
+
+function change_11pro() {
+  var change_11pro = document.querySelector('.iPhone-11pro-moreinfo');
+  change_11pro.style.textDecoration = "underline";
+}
+
+function origin_11pro() {
+  var change_11pro = document.querySelector('.iPhone-11pro-moreinfo');
+  change_11pro.style.textDecoration = "none";
+}
+
+function change_11probuy() {
+  var change_11probuy = document.querySelector('.iPhone-11pro-buy-part');
+  change_11probuy.style.textDecoration = "underline";
+}
+
+function origin_11probuy() {
+  var change_11probuy = document.querySelector('.iPhone-11pro-buy-part');
+  change_11probuy.style.textDecoration = "none";
+}
+
+function change_watch() {
+  var change_watch = document.querySelector('.Watch-moreinfo');
+  change_watch.style.textDecoration = "underline";
+}
+
+function origin_watch() {
+  var change_watch = document.querySelector('.Watch-moreinfo');
+  change_watch.style.textDecoration = "none";
+}
+
+function change_watchbuy() {
+  var change_watchbuy = document.querySelector('.Watch-buy-part');
+  change_watchbuy.style.textDecoration = "underline";
+}
+
+function origin_watchbuy() {
+  var change_watchbuy = document.querySelector('.Watch-buy-part');
+  change_watchbuy.style.textDecoration = "none";
+}
+
+function change_myunit() {
+  var change_myunit = document.querySelector('.myunit-moreinfo');
+  change_myunit.style.textDecoration = "underline";
+}
+
+function origin_myunit() {
+  var change_myunit = document.querySelector('.myunit-moreinfo');
+  change_myunit.style.textDecoration = "none";
+}
+
+function change_myunitbuy() {
+  var change_myunitbuy = document.querySelector('.myunit-buy-part');
+  change_myunitbuy.style.textDecoration = "underline";
+}
+
+function origin_myunitbuy() {
+  var change_myunitbuy = document.querySelector('.myunit-buy-part');
+  change_myunitbuy.style.textDecoration = "none";
+}
+
+function change_gotostore() {
+  var change_gotostore = document.querySelector('.goto-store');
+  change_gotostore.style.textDecoration = "underline";
+}
+
+function origin_gotostore() {
+  var change_gotostore = document.querySelector('.goto-store');
+  change_gotostore.style.textDecoration = "none";
+}
+
+function change_reseller() {
+  var change_reseller = document.querySelector('.reseller');
+  change_reseller.style.textDecoration = "underline";
+}
+
+function origin_reseller() {
+  var change_reseller = document.querySelector('.reseller');
+  change_reseller.style.textDecoration = "none";
+}
